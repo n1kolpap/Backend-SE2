bc89a2f44afdbee6162a2ad25ea36a881b1128e8
BopReznole <bopreznole@protonmail.com>
2026-01-09 01:41:11 +0200

    adjust max VUs - final
    createTripPlan: load/spike: 1024/496
    
    login: load/spike: 3072/3840

--- tests/k6/createTripPlan-spike-test.js
+++ tests/k6/createTripPlan-spike-test.js
@@ -35,7 +35,7 @@ import { check, sleep } from "k6";
  * Tunable variables (env-driven)
  * ----------------------------- */
 const BASE_URL = __ENV.BASE_URL || "http://localhost:3000";
-const MAX_VUS = Number(__ENV.MAX_VUS || 512);
+const MAX_VUS = Number(__ENV.MAX_VUS || 496);
 
 // “Max duration during a ramping stage”
 const RAMP_STAGE_DURATION = __ENV.RAMP_STAGE_DURATION || "10s";
